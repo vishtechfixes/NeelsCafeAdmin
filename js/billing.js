a9ea1538d299ee6c309c5a38f8e46bff7eada925
vishtechfixes <vishtechfixes@gmail.com>
2026-07-14 11:37:23 +0530

    Update billing.jsvvvvvvishnu

--- js/billing.js
+++ js/billing.js
@@ -349,7 +349,7 @@ window.cbWhatsAppReceipt = function() {
   if (!cbLastBill) return;
   const b  = cbLastBill;
   const sh = JSON.parse(localStorage.getItem(LS.shop) || '{}');
-  const appLink = 'https://neels.vishtechfixes.com';
+  const appLink = 'https://neelscafe-customers.vishtechfixes.com/';
   const lines = b.items.map(i =>
     `${i.name}${i.variant?' ('+i.variant+')':''} x${i.qty} = ₹${i.price*i.qty}`
   ).join('\n');
